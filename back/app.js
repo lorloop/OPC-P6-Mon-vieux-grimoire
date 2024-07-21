14e88e6d32c0cd71b9cf960619e3d4e978ab8e1d
lorloop <gallet.julien@orange.fr>
2024-07-22 00:26:10 +0200

    routes auth

--- back/app.js
+++ back/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const authRoutes = require('./routes/auth');
 
-mongoose.connect(process.env.MONGODB_URL)
+mongoose.connect('mongodb+srv://mvg:AqOE13xgbu9eYYad@mvg-db.evagwgp.mongodb.net/?retryWrites=true&w=majority&appName=MVG-DB')
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
+
+app.use(express.json());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', process.env.CORS_ALLOWED_ORIGINS);
@@ -14,6 +17,7 @@ app.use((req, res, next) => {
     next();
 });
 
-//app.use('/api/t&a', trucsEtAstucesRoutes);
+
+app.use('/api/auth', authRoutes);
 
 module.exports = app;
